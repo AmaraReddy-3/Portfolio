f96b5d5bfc3f4efffe544bc06726bdaffd108068
Amara Reddy Chinnapareddy <amarareddy7075@gmail.com>
2025-05-13 10:35:31 +0530

    Another update

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
+// JavaScript to toggle the nav menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
 var typed = new Typed(".text", {
     strings:["Frontend Developer", "Backend Developer", "DataBase Management"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
+
 });
