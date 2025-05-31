49f3a44e78b679ef78c3d85d5deab02fd86b09c5
Amara Reddy Chinnapareddy <amarareddy7075@gmail.com>
2025-05-31 15:49:16 +0530

    Initial Commit For mobile Responsive of Portfolio

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-
 // JavaScript to toggle the nav menu
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".navbar");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
 var typed = new Typed(".text", {
-    strings:["Frontend Developer", "Backend Developer", "DataBase Management"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-
+  strings: ["Frontend Developer", "Backend Developer", "FullStack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
